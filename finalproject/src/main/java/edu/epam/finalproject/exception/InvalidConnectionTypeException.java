2d5bb7f5fee2fd3de9f7815b24eb14edde1336a1
Anna Bulanchuk <anabulancuk@gmail.com>
2021-12-09 22:12:25 +0300

    add pool connections

---
+++ finalproject/src/main/java/edu/epam/finalproject/exception/InvalidConnectionTypeException.java
@@ -0,0 +1,7 @@
+package edu.epam.finalproject.exception;
+
+public class InvalidConnectionTypeException extends Exception{
+    public InvalidConnectionTypeException(String message){
+        super(message);
+    }
+}
